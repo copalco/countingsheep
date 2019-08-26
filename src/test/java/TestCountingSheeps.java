4e678ab6b91c1e411d71ec5f7c31cb35c113470d
Piotr Kopalko <copalco@gmail.com>
2019-08-27 00:54:23 +0200

    Add parameter to countSheeps method.

--- src/test/java/TestCountingSheeps.java
+++ src/test/java/TestCountingSheeps.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.assertEquals;
 public class TestCountingSheeps {
     @Test
     public void countsSheeps() {
-        assertEquals(countSheeps(), 0);
+        Boolean[] sheeps = {};
+        assertEquals(countSheeps(sheeps), 0);
     }
 
-    private int countSheeps() {
+    private int countSheeps(Boolean[] sheeps) {
         return 0;
     }
 }
