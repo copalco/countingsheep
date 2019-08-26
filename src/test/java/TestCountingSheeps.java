da3701a23de2c9a2c20778f8cd3cfa7381e6b4c6
Piotr Kopalko <copalco@gmail.com>
2019-08-27 00:41:45 +0200

    Initial commit

---
+++ src/test/java/TestCountingSheeps.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCountingSheeps {
+    @Test
+    public void countsSheeps() {
+        assertEquals(countSheeps(), 0);
+    }
+
+    private int countSheeps() {
+        return 0;
+    }
+}
