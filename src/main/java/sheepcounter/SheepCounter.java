f5cae98348ffd59fc0e4f77d75b1afc2bcac90cb
Piotr Kopalko <copalco@gmail.com>
2019-08-27 10:50:55 +0200

    Transform array to stream.

--- src/main/java/sheepcounter/SheepCounter.java
+++ src/main/java/sheepcounter/SheepCounter.java
@@ -1,11 +1,15 @@
 package sheepcounter;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class SheepCounter {
     public int countSheeps(Boolean[] arrayOfSheep) {
         if (arrayOfSheep == null) {
             return 0;
         }
         int sheepCounter = 0;
+        Stream<Boolean> sheepStream = Arrays.stream(arrayOfSheep);
         for (Boolean sheep : arrayOfSheep) {
             if (sheep != null && sheep) {
                 sheepCounter += 1;
