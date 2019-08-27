fea334e36a96a5f07a23a1cf91a329c781b159d8
Piotr Kopalko <copalco@gmail.com>
2019-08-27 10:18:14 +0200

    Rename test method.

--- src/test/java/TestCountingSheeps.java
+++ src/test/java/TestCountingSheeps.java
@@ -6,25 +6,25 @@ import static org.junit.Assert.assertEquals;
 public class TestCountingSheeps {
     @Test
     public void noSheeps() {
-        assertEquals(countSheeps(new Boolean[]{}), 0);
+        assertEquals(countSheep(new Boolean[]{}), 0);
     }
 
     @Test
     public void countTrueAsSheep() {
-        assertEquals(countSheeps(new Boolean[]{true, true, true}), 3);
+        assertEquals(countSheep(new Boolean[]{true, true, true}), 3);
     }
 
     @Test
     public void doesNotCountFalse() {
-        assertEquals(countSheeps(new Boolean[]{false, false, false}), 0);
+        assertEquals(countSheep(new Boolean[]{false, false, false}), 0);
     }
 
     @Test
     public void countsNullArrayAsZero() {
-        assertEquals(countSheeps(null), 0);
+        assertEquals(countSheep(null), 0);
     }
 
-    private int countSheeps(Boolean[] sheep) {
+    private int countSheep(Boolean[] sheep) {
         SheepCounter counter = new SheepCounter();
         return counter.countSheeps(sheep);
     }
