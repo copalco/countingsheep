e674869f5d030d93d51589972da54782c4da8a04
Piotr Kopalko <copalco@gmail.com>
2019-08-27 10:33:59 +0200

    Early return for null.

--- src/main/java/sheepcounter/SheepCounter.java
+++ src/main/java/sheepcounter/SheepCounter.java
@@ -3,11 +3,12 @@ package sheepcounter;
 public class SheepCounter {
     public int countSheeps(Boolean[] arrayOfSheep) {
         int sheepCounter = 0;
-        if (arrayOfSheep != null) {
-            for (Boolean sheep : arrayOfSheep) {
-                if (sheep != null && sheep) {
-                    sheepCounter += 1;
-                }
+        if (arrayOfSheep == null) {
+            return 0;
+        }
+        for (Boolean sheep : arrayOfSheep) {
+            if (sheep != null && sheep) {
+                sheepCounter += 1;
             }
         }
         return sheepCounter;
