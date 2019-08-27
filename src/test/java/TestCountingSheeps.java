5a62f6581398d95bc7eb769a0ce9d496ce5a3e1b
Piotr Kopalko <copalco@gmail.com>
2019-08-27 10:30:42 +0200

    Inline counting sheep in the tests.

--- src/test/java/TestCountingSheeps.java
+++ src/test/java/TestCountingSheeps.java
@@ -9,30 +9,27 @@ public class TestCountingSheeps {
 
     @Test
     public void noSheep() {
-        assertEquals(countSheep(new Boolean[]{}), 0);
+        assertEquals(counter.countSheeps(new Boolean[]{}), 0);
     }
 
     @Test
     public void countTrueAsSheep() {
-        assertEquals(countSheep(new Boolean[]{true, true, true}), 3);
+        assertEquals(counter.countSheeps(new Boolean[]{true, true, true}), 3);
     }
 
     @Test
     public void doesNotCountFalse() {
-        assertEquals(countSheep(new Boolean[]{false, false, false}), 0);
+        assertEquals(counter.countSheeps(new Boolean[]{false, false, false}), 0);
     }
 
     @Test
     public void countsNullArrayAsZero() {
-        assertEquals(countSheep(null), 0);
+        assertEquals(counter.countSheeps(null), 0);
     }
 
     @Test
     public void ignoresNulls() {
-        assertEquals(countSheep(new Boolean[]{null}), 0);
+        assertEquals(counter.countSheeps(new Boolean[]{null}), 0);
     }
 
-    private int countSheep(Boolean[] sheep) {
-        return counter.countSheeps(sheep);
-    }
 }
