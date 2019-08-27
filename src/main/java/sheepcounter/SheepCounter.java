7bf429369c572e5587a949bbbf4d17f4dc3575d9
Piotr Kopalko <copalco@gmail.com>
2019-08-27 10:38:06 +0200

    Move variable declaration right before its usage.

--- src/main/java/sheepcounter/SheepCounter.java
+++ src/main/java/sheepcounter/SheepCounter.java
@@ -2,10 +2,10 @@ package sheepcounter;
 
 public class SheepCounter {
     public int countSheeps(Boolean[] arrayOfSheep) {
-        int sheepCounter = 0;
         if (arrayOfSheep == null) {
             return 0;
         }
+        int sheepCounter = 0;
         for (Boolean sheep : arrayOfSheep) {
             if (sheep != null && sheep) {
                 sheepCounter += 1;
